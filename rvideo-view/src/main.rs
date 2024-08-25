c856765ff8b61cb6f8fc659a9d7476cdb274d4b0
Serhij S <s@g5.is>
2024-08-25 03:16:06 +0200

    win fixes

--- rvideo-view/src/main.rs
+++ rvideo-view/src/main.rs
@@ -1,6 +1,3 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-#![allow(rustdoc::missing_crate_level_docs)]
-
 use std::{
     sync::{
         mpsc::{channel, Receiver, Sender},
